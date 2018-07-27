3b59bd760d26999b96da80e0c299df8d04c7675c
laisxn <191120844@qq.com>
2018-07-27 18:09:27 +0800

    webchat

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+require.config({
+    baseUrl : 'js',
+    paths : {
+        vue : ["https://cdn.jsdelivr.net/npm/vue/dist/vue.js", "vue"],
+        jquery : ["http://libs.baidu.com/jquery/2.0.3/jquery", 'jquery-1.8.0.min'],
+    }
+})
