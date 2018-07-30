c285bde4f416cc6fdff7cf92c0ddd511a3abba29
laisxn <191120844@qq.com>
2018-07-30 17:45:13 +0800

    webchat

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,8 @@
 require.config({
     baseUrl : 'js',
     paths : {
-        vue : ["https://cdn.jsdelivr.net/npm/vue/dist/vue.js", "vue"],
+        vue : ["https://cdn.jsdelivr.net/npm/vue/dist/vue", "vue"],
         jquery : ["http://libs.baidu.com/jquery/2.0.3/jquery", 'jquery-1.8.0.min'],
+        ws: ["socket"]
     }
 })
---
+++ public/js/socket.js
@@ -0,0 +1,34 @@
+function webSocket()
+{
+    if ("WebSocket" in window)
+    {
+        alert("您的浏览器支持 WebSocket!");
+
+        var ws = new WebSocket("ws://119.23.237.171:9501");
+
+        ws.onopen = function()
+        {
+            ws.send("发送数据123");
+            alert("数据发送中...");
+        };
+
+        ws.onmessage = function (evt)
+        {
+            var received_msg = evt.data;
+            console.log(evt);
+            alert("数据已接收...");
+        };
+
+        ws.onclose = function()
+        {
+            // 关闭 websocket
+            alert("连接已关闭...");
+        };
+    }
+
+    else
+    {
+        // 浏览器不支持 WebSocket
+        alert("您的浏览器不支持 WebSocket!");
+    }
+}
