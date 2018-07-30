d8104049ab7b705ecd00a9694a4e1bc50c09a907
laisxn <191120844@qq.com>
2018-07-30 17:53:54 +0800

    webchat

--- public/js/socket.js
+++ public/js/socket.js
@@ -2,7 +2,7 @@ function webSocket()
 {
     if ("WebSocket" in window)
     {
-        alert("您的浏览器支持 WebSocket!");
+        console.log("您的浏览器支持 WebSocket!");
 
         var ws = new WebSocket("ws://119.23.237.171:9501");
 
